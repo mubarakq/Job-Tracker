cc3580020aed3bc3f5881bb0773b9d0ffa65fc02
Adio Mubarak <adiomubby@gmail.com>
2025-09-23 10:45:36 +0100

    adio sect

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+
+function Index() {
+  return (
+    <div className="w-full h-dvh bg-amber-500 p-2.5">
+        <h1 className="text-2xl text-blue-950">Index</h1>
+    </div>
+  )
+}
+
+export default Index
